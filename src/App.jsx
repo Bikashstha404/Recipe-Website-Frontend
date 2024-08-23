c68b9c572780012b3b2236f95457b98f9a739814
Bikash Shrestha <stha.bikam99@gmail.com>
2024-08-23 15:02:50 +0545

    setup of Shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <>
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+    <>
+     <Button>Secondary</Button> <br /><br />
+     <Button variant="destructive">Here</Button> <br /><br />
+     <Button variant="secondary">Secondary</Button> <br /><br />
+     <Button variant="ghost">Secondary</Button> <br /><br />
+   </>
     </>
   )
 }
