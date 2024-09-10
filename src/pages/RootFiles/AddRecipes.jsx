20220123c9c274b91f1f0fa2a2e2068cc8b523d3
Bikash Shrestha <stha.bikam99@gmail.com>
2024-09-10 23:12:18 +0545

    Upto to preparation time done

--- src/pages/RootFiles/AddRecipes.jsx
+++ src/pages/RootFiles/AddRecipes.jsx
@@ -1,247 +1,251 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage, faPlus } from "@fortawesome/free-solid-svg-icons";
+import InputMask from "react-input-mask";
 
 export default function AddRecipes() {
-  const categories = [
-    { value: "desserts", label: "Desserts" },
-    { value: "main-dishes", label: "Main Dishes" },
-    // Add more categories as needed
-  ];
+  const [recipeFormData, setRecipeFormData] = useState({
+    title: "",
+    description: "",
+    prepTime: "",
+    calories: "",
+    category: "",
+    subCategory: "",
+    ingredients: [{ quantity: "", name: "" }],
+    preparation: "",
+    imagePath: "",
+  });
 
-  const subCategories = {
-    desserts: [
-      { value: "cakes", label: "Cakes" },
-      { value: "cookies", label: "Cookies" },
-      // Add more sub-categories as needed
-    ],
-    "main-dishes": [
-      { value: "pasta", label: "Pasta" },
-      { value: "steak", label: "Steak" },
-      // Add more sub-categories as needed
-    ],
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!recipeFormData.title) newErrors.title = "*Recipe title is required";
+    if (!recipeFormData.description)
+      newErrors.description = "*Description is required";
+
+    if (!image) {
+      newErrors.image = "*Image is required";
+    }
+
+    if (!recipeFormData.prepTime) {
+      newErrors.prepTime = "*Preparation time is required.";
+    }
+    const [hours, minutes, seconds] = recipeFormData.prepTime
+      .split(":")
+      .map(Number);
+    
+    if(hours== 0 && minutes == 0 && seconds == 0){
+        newErrors.prepTime = "*Invalid Time"
+    }
+    if (
+      hours < 0 ||
+      hours > 23 ||
+      minutes < 0 ||
+      minutes > 59 ||
+      seconds < 0 ||
+      seconds > 59
+    ) {
+      newErrors.prepTime = "*Invalid time format.";
+    }
+
+    setErrors(newErrors);
+    setTimeout(() => {
+      setErrors({});
+    }, 5000);
+
+    return Object.keys(newErrors).length === 0;
   };
 
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [subCategoryOptions, setSubCategoryOptions] = useState([]);
-  const [selectedSubCategory, setSelectedSubCategory] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  useEffect(() => {
-    if (selectedCategory) {
-      setSubCategoryOptions(subCategories[selectedCategory] || []);
+    if (validate()) {
+      console.log("Form submitted successfully:", recipeFormData);
+      //   try {
+      //     const response = await fetch("http://localhost:5289/Recipe/AddRecipe", {
+      //       method: "POST",
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //       },
+      //       body: JSON.stringify(recipeFormData),
+      //     });
+      //     const data = await response.json();
+      //     console.log(data);
+      //   } catch (error) {
+      //     console.error("There was an error posting the data!", error);
+      //   }
     } else {
-      setSubCategoryOptions([]);
+      console.log("Form submission failed due to validation errors.");
     }
-  }, [selectedCategory]);
+  };
 
-  const [ingredients, setIngredients] = useState([{ quantity: "", name: "" }]);
-  const addIngredient = () => {
-    setIngredients([...ingredients, { quantity: "", name: "" }]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipeFormData({
+      ...recipeFormData,
+      [name]: value,
+    });
+  };
+
+  const [imagePreview, setImagePreview] = useState(null);
+  const [image, setImage] = useState(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage({
+      ...image,
+      image: file,
+    });
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+      setRecipeFormData({
+        ...recipeFormData,
+        imagePath: reader.result,
+      });
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   };
 
   return (
-    <main className="mx-auto pt-5 pb-8 w-full max-w-7xl">
-      <h2 className="text-4xl font-bold mb-8 ml-11 underline">Add Recipes</h2>
-      <form>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-          <div>
-            <div className="flex flex-col mb-4">
-              <label
-                className="text-gray-700 text-lg font-bold ml-11 mb-1"
-                htmlFor="recipe-title"
-              >
-                Recipe Title
-              </label>
-              <input
-                type="text"
-                id="recipe-title"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-11"
-                placeholder="Enter recipe title"
-              />
-            </div>
-
-            <div className="flex flex-col mb-4">
-              <label
-                className="text-gray-700 text-lg font-bold ml-11 mb-1"
-                htmlFor="description"
-              >
-                Description
-              </label>
-              <textarea
-                id="description"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-11"
-                placeholder="Enter recipe description"
-                rows="6"
-                style={{ height: "150px", overflow: "auto" }}
-              ></textarea>
-            </div>
-          </div>
-
-          <div className="flex justify-center items-center">
-            <label
-              htmlFor="recipe-image"
-              className="flex flex-col justify-center items-center w-4/5 h-64 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer hover:bg-gray-100"
-            >
-              <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                <FontAwesomeIcon
-                  icon={faImage}
-                  className="text-3xl text-gray-800"
+    <>
+      <main className="mx-auto pt-5 pb-8 w-full max-w-7xl">
+        <h2 className="text-4xl font-bold mb-8 ml-11 underline">Add Recipes</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+            <div>
+              <div className="flex flex-col mb-4">
+                <label
+                  className="text-gray-700 text-lg font-bold ml-11 mb-1"
+                  htmlFor="recipe-title"
+                >
+                  Recipe Title
+                </label>
+                <input
+                  type="text"
+                  id="recipe-title"
+                  name="title"
+                  value={recipeFormData.title}
+                  onChange={handleChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-11"
+                  placeholder="Enter recipe title"
                 />
-                <p className="mb-2 text-sm text-gray-500">
-                  <i className="fas fa-upload"> Click icon to add picture</i>
-                </p>
+                {errors.title && (
+                  <small className="text-red-500 font-semibold ml-11">
+                    {errors.title}
+                  </small>
+                )}
               </div>
-              <input id="recipe-image" type="file" className="hidden" />
-            </label>
-          </div>
-        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-6 ml-11">
-          <div>
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="prep-time"
-            >
-              Prep Time
-            </label>
-            <input
-              type="text"
-              id="prep-time"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter prep time"
-            />
-          </div>
-          <div>
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2 ml-4"
-              htmlFor="calories"
-            >
-              Calories
-            </label>
-            <input
-              type="number"
-              id="calories"
-              className="ml-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter calories"
-            />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-6 ml-11">
-          <div>
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="category"
-            >
-              Category
-            </label>
-            <select
-              id="category"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={selectedCategory}
-              onChange={(e) => setSelectedCategory(e.target.value)}
-            >
-              <option value="">Select category</option>
-              {categories.map((cat) => (
-                <option key={cat.value} value={cat.value}>
-                  {cat.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2 ml-4"
-              htmlFor="sub-category"
-            >
-              Sub-Category
-            </label>
-            <select
-              id="sub-category"
-              className="ml-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={selectedSubCategory}
-              onChange={(e) => setSelectedSubCategory(e.target.value)}
-              disabled={!selectedCategory}
-            >
-              <option value="">Select sub-category</option>
-              {subCategoryOptions.map((subCat) => (
-                <option key={subCat.value} value={subCat.value}>
-                  {subCat.label}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="mb-6 ml-11">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Ingredients
-          </label>
-
-          {ingredients.map((ingredient, index) => (
-            <div
-              key={index}
-              className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 items-center"
-            >
-              <input
-                type="text"
-                placeholder="Quantity"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                value={ingredient.quantity}
-                onChange={(e) =>
-                  handleIngredientChange(index, "quantity", e.target.value)
-                }
-              />
-
-              <input
-                type="text"
-                placeholder="Name"
-                className="ml-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                value={ingredient.name}
-                onChange={(e) =>
-                  handleIngredientChange(index, "name", e.target.value)
-                }
-              />
+              <div className="flex flex-col mb-4">
+                <label
+                  className="text-gray-700 text-lg font-bold ml-11 mb-1"
+                  htmlFor="description"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  name="description"
+                  value={recipeFormData.description}
+                  onChange={handleChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-11"
+                  placeholder="Enter recipe description"
+                  rows="6"
+                  style={{ height: "150px", overflow: "auto" }}
+                ></textarea>
+                {errors.description && (
+                  <small className="text-red-500 font-semibold ml-11">
+                    {errors.description}
+                  </small>
+                )}
+              </div>
             </div>
-          ))}
 
-          <button
-            type="button"
-            onClick={addIngredient}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-72"
-          >
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-
-          <div>
-            <div style={{ position: "relative", bottom: "41vh", left: "44vw" }}>
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Preparation
-              </label>
-              <textarea
-                id="preparation"
-                className="shadow appearance-none border rounded w-2/5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Enter preparation steps"
-                rows="10"
-              ></textarea>
+            <div>
+              <div className="flex justify-center items-center">
+                <label
+                  htmlFor="recipe-image"
+                  className="flex flex-col justify-center items-center w-4/5 h-64 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer hover:bg-gray-100"
+                >
+                  <div className="flex flex-col justify-center items-center">
+                    {imagePreview ? (
+                      <img
+                        src={imagePreview}
+                        alt="Preview"
+                        className="rounded-lg w-[510px] h-[255px]"
+                      />
+                    ) : (
+                      <>
+                        <FontAwesomeIcon
+                          icon={faImage}
+                          className="text-3xl text-gray-800"
+                        />
+                        <p className="mb-2 text-sm text-gray-500">
+                          <i className="fas fa-upload">
+                            {" "}
+                            Click icon to add picture
+                          </i>
+                        </p>
+                      </>
+                    )}
+                  </div>
+                  <input
+                    id="recipe-image"
+                    type="file"
+                    className="hidden"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                  />
+                </label>
+              </div>
+              {errors.image && (
+                <small className="text-red-500 font-semibold ml-11">
+                  {errors.image}
+                </small>
+              )}
             </div>
-            <div className="preparationBox flex justify-end space-x-4 mr-16" style={{position: 'relative', bottom: '41vh'}}>
-              <button
-                type="button"
-                className="bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Discard
-              </button>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6 ml-11">
+              <div>
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="prep-time"
+                >
+                  Prep Time
+                </label>
+                <InputMask
+                  mask="99:99:99"
+                  name="prepTime"
+                  type="text"
+                  id="prep-time"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Enter prep time (hh:mm:ss)"
+                  value={recipeFormData.prepTime}
+                  onChange={handleChange}
+                />
+                {errors.prepTime && (
+                  <small className="text-red-500 font-semibold mt-2 block">
+                    {errors.prepTime}
+                  </small>
+                )}
+              </div>
+            </div>
+            <div className="preparationBox flex justify-end space-x-4 mr-16">
               <button
                 type="submit"
                 className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={handleSubmit}
               >
                 Add
               </button>
             </div>
           </div>
-        </div>
-      </form>
-    </main>
+        </form>
+      </main>
+    </>
   );
 }
