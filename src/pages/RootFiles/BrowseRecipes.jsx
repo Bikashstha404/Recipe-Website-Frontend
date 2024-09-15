66fcbfa70f5a71f5fa7862da2ec24c197acd90d8
Bikash Shrestha <stha.bikam99@gmail.com>
2024-09-15 22:15:00 +0545

    Browse Recipes Data Shown

--- src/pages/RootFiles/BrowseRecipes.jsx
+++ src/pages/RootFiles/BrowseRecipes.jsx
@@ -1,43 +1,61 @@
 import React from "react";
 import Food from "../../assets/Burger.webp";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 export default function BrowseRecipes() {
+  const [recipeData, setRecipeData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5289/api/Recipe/GetAllRecipes")
+      .then((apiData) => {
+        setRecipeData(apiData.data);
+        console.log(apiData.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <main className="mx-auto pt-5 pb-8 w-full max-w-7xl">
       <h2 className="text-4xl font-bold mb-8 ml-11 underline">Recipes</h2>
       <div className="flex flex-col container mx-auto pt-5 pb-8 w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4">
-          {[...Array(9)].map((_, i) => (
-            <div
-              key={i}
-              className="border rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-95"
-            >
-              <img
-                src={Food}
-                alt="Food"
-                className="w-full h-48 object-cover"
-                loading="lazy"
-              />
-              <div className="p-3 bg-gray-50">
-                <h3 className="text-base font-semibold">Burger</h3>
-                <p className="text-xs text-gray-600">
-                  Category:{" "}
-                  <span className="font-medium">Entrees, Sandwiches</span>
-                </p>
-                <p className="mt-2 text-xs text-justify truncate-3-lines">
-                  This juicy, flavorful burger is made with high-quality beef,
-                  caramelized onions, fresh lettuce, and a special homemade
-                  sauce, all served in a toasted bun. Perfect for a satisfying
-                  meal! This is just a checkup if the text overflows or not.
-                </p>
-                <div className="flex justify-end mt-2">
-                  <button className="px-3 py-1 text-xs font-semibold text-white bg-orange-400 rounded-lg hover:bg-orange-500 transition-colors">
-                    See Recipe
-                  </button>
+          {recipeData.length > 0 ? (
+            recipeData.map((recipe) => (
+              <div
+                key={recipe.id}
+                className="border rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-95 h-full flex flex-col justify-between"
+              >
+                <img
+                  src={recipe.imageUrl}
+                  alt="Food"
+                  className="w-full h-48 object-cover"
+                  loading="lazy"
+                />
+                <div className="p-3 bg-gray-50 flex flex-col flex-grow">
+                  <h3 className="text-base font-semibold">{recipe.title}</h3>
+                  <p className="text-xs text-gray-600">
+                    Main Category:
+                    <span className="font-medium">{recipe.mainCategory}</span>
+                    <br />
+                    Sub Category:
+                    <span className="font-medium">{recipe.subCategory}</span>
+                  </p>
+                  <p className="mt-2 text-xs text-justify truncate-3-lines flex-grow">
+                    {recipe.description}
+                  </p>
+                  <div className="flex justify-end mt-3">
+                    <button className="px-3 py-1 text-xs font-semibold text-white bg-orange-400 rounded-lg hover:bg-orange-500 transition-colors">
+                      See Recipe
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <div>No Recipes Available</div>
+          )}
         </div>
       </div>
     </main>
